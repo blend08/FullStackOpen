05859130a0dcd4e09f3f7075e46a889fb03b0b58
blend08 <blenderrrr08@gmail.com>
2022-11-15 13:38:53 +0800

    Exercise 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,17 +1,47 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
+
+const Weather = ({lat, lon}) => {
+  const [weather, setWeather] = useState([])
+  const params = {
+    lat: lat,
+    lon: lon,
+    appid: process.env.REACT_APP_API_KEY,
+    units: "metric"
+  }
+
+  useEffect(() => {axios.get('https://api.openweathermap.org/data/2.5/weather', {params})
+                        .then(response => setWeather(response.data))},[])
+
+  console.log(weather)
+  return (
+    <>
+      {Object.keys(weather).length === 0 ? <div></div>
+        :<>
+          <div>temperature: {weather.main.temp} Celcius</div>
+          <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+          <div>wind: {weather.wind.speed} m/s</div>
+        </>
+      }
+    </>
+  )
+
+}  
+
 function App() {
   const [countries, setCountries] = useState([])
   const [search, setSearch] = useState('')
   const filteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(search.toLowerCase()))
+  const api_key = "0e40599bd04dedcd36f48db337fcbaba"
 
   useEffect(() => {axios.get('https://restcountries.com/v3.1/all').then(response => setCountries(response.data))}, [])
+
+  
 
   const handleSearch = (event) => {
     setSearch(event.target.value)
   }
-
 
   return (
     <div>
@@ -23,6 +53,7 @@ function App() {
                 <h1>{country.name.common}</h1>
                 <div>capital: {country.capital}</div>
                 <div>area : {country.area}</div>
+                <div>latlng: {country.capitalInfo.latlng[0]}</div>
                 <h4>languages: </h4>
                 <ul>
                   {Object.values(country.languages).map((language,id) => 
@@ -30,6 +61,8 @@ function App() {
                   )}
                 </ul>
                 <img src={country.flags.png} />
+                <h2>Weather in {country.name.common}</h2>
+                <Weather lat={country.capitalInfo.latlng[0]} lon={country.capitalInfo.latlng[1]} />
               </div>
             )}
           </div>
