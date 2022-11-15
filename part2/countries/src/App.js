66ec74363c1f9bbb078e092c3e5804813b4941d4
blend08 <blenderrrr08@gmail.com>
2022-11-15 11:15:26 +0800

    Exercise 2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,46 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-const Countries = ({countries, showData}) => {
-  return(
-    <>
-      {countries.map((country, id) => 
-        <div key={id}>
-          {showData ? <h1>{country.name.common}</h1> : <div>{country.name.common}</div>}
-          {showData && 
-            <>
-              <div>capital: {country.capital}</div>
-              <div>area: {country.area}</div>
-              <h4>languages: </h4>
-              <ul>
-                {Object.values(country.languages).map((language,index) =>
-                  <li key={index}>{language}</li>
-                )}
-              </ul>
-              <img src={country.flags.png} />              
-            </>
-          }
-        </div>  
-      )}
-    </>
-  )
-}
-
-const Content = ({countries}) => { 
-  if (countries.length === 1) {
-    return(
-      <>
-        <Countries countries={countries} showData={true} />
-      </>
-    )
-  }
-
-  else if(countries.length > 10) return <div>Too many matches, specify another filter</div>
-
-  else return <Countries countries={countries} />
-
-}
-
 function App() {
   const [countries, setCountries] = useState([])
   const [search, setSearch] = useState('')
@@ -52,12 +12,40 @@ function App() {
     setSearch(event.target.value)
   }
 
+
   return (
     <div>
       <div>find countries <input value={search} onChange={handleSearch}/></div>
-      <Content countries={filteredCountries} />
+      {filteredCountries.length === 1 ? 
+          <div>
+            {filteredCountries.map((country,id) =>
+              <div key={id}>
+                <h1>{country.name.common}</h1>
+                <div>capital: {country.capital}</div>
+                <div>area : {country.area}</div>
+                <h4>languages: </h4>
+                <ul>
+                  {Object.values(country.languages).map((language,id) => 
+                    <li key={id}>{language}</li>
+                  )}
+                </ul>
+                <img src={country.flags.png} />
+              </div>
+            )}
+          </div>
+        : filteredCountries.length > 10 ? <div>Too many matches, specify another filter</div>
+        : <div>
+          {filteredCountries.map((country,id) => 
+            <div key={id}>
+              {country.name.common} <button value={country.name.common} onClick={handleSearch}>show</button>
+            </div> 
+          )}
+        </div>
+      }
+      
     </div>
   );
 }
 
 export default App;
+
