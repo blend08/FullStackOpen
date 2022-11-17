58560b7559bd7e1fd3b6be16728cde0718448473
blend08 <blenderrrr08@gmail.com>
2022-11-17 17:09:35 +0800

    Exercise 3.6

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -65,6 +65,20 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
+    if(!body.name) {
+        return response.status(400).json({
+            error: 'name is missing'
+          })
+    } else if (!body.number) {
+        return response.status(400).json({
+            error: 'number is missing'
+        })
+    } else if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
     const person = {
         id: generateId(),
         name: body.name,
