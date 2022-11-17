d79d30a0dbd56c0601ef9a3186d99fa744b23b1c
blend08 <blenderrrr08@gmail.com>
2022-11-17 17:01:52 +0800

    Exercise 3.5

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,5 +1,8 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 let persons = [
     { 
@@ -53,6 +56,24 @@ app.delete('/api/person/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+const generateId = () => {
+    return Math.floor(Math.random() * 9999999999)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(persons)
 })
 
 const PORT = 3001
